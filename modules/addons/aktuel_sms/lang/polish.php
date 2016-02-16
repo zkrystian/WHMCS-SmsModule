8e047068b56fd66af2bf5ee990db989a01313207
Krystian <zkrystian@gmail.com>
2016-02-16 09:30:09 +0100

    Added polish translation
    Added polish translation

---
+++ modules/addons/aktuel_sms/lang/polish.php
@@ -0,0 +1,48 @@
+﻿<?php
+$_ADDONLANG['lang'] = "polish";
+
+$_ADDONLANG['newversion'] = "There is an update for addon. Please visit <a href=\"https://github.com/AktuelSistem/WHMCS-SmsModule\">Github page</a> for download new version.<br><br>Please visit this page after file transfer finished.<br><br>";
+$_ADDONLANG['uptodate'] = "Your addon is up to date.";
+$_ADDONLANG['save'] = "Zapisz";
+$_ADDONLANG['gsmnumberfield'] = "GSM/Mobile Number Field (Custom client field)";
+$_ADDONLANG['wantsmsfield'] = "Subscribe SMS Field (Custom client field)";
+$_ADDONLANG['user'] = "Username";
+$_ADDONLANG['pass'] = "Password";
+$_ADDONLANG['apiid'] = "API ID";
+$_ADDONLANG['senderid'] = "SenderID(Title)";
+$_ADDONLANG['dateformat'] = "Date Format";
+$_ADDONLANG['sender'] = "Sender";
+$_ADDONLANG['settings'] = "Ustawienia";
+$_ADDONLANG['signature'] = "Podpis";
+$_ADDONLANG['clientsmstemplates'] = "Szablony klienta";
+$_ADDONLANG['adminsmstemplates'] = "Szablony admina";
+$_ADDONLANG['sendsms'] = "Wyślij SMS";
+$_ADDONLANG['messages'] = "Wyślij wiadomości";
+$_ADDONLANG['update'] = "Aktualizuj";
+$_ADDONLANG['smssent'] = "<br><br><b>SMS wysłany</b>";
+$_ADDONLANG['client'] = "Klient";
+$_ADDONLANG['selectclient'] = "Wybierz klienta";
+$_ADDONLANG['message'] = "Wiadomość";
+$_ADDONLANG['debug'] = "Pokaż logi";
+$_ADDONLANG['send'] = "Wyślij";
+$_ADDONLANG['debugsonuc'] = "Wynik";
+$_ADDONLANG['gsmnumber'] = "Numer tel";
+$_ADDONLANG['datetime'] = "Date Time";
+$_ADDONLANG['status'] = "Status";
+$_ADDONLANG['delete'] = "Delete";
+$_ADDONLANG['parameter'] = "Parameter:";
+$_ADDONLANG['active'] = "Activate";
+$_ADDONLANG['ekstra'] = "Extra {x}";
+$_ADDONLANG['admingsm'] = "Admin GSM Numbers";
+$_ADDONLANG['admingsmornek'] = "Seperate numbers with comma Etc. 5321232525,5331002020";
+$_ADDONLANG['lisans'] = "<a href=\"https://github.com/AktuelSistem/WHMCS-SmsModule\">Github Page</a>";
+$_ADDONLANG['credit'] = "Pozostała liczba sms";
+
+$_ADDONLANG['error'] = "Error";
+$_ADDONLANG['success'] = "Success";
+
+$_ADDONLANG['authkey'] = "Authentication Key";
+$_ADDONLANG['route'] = "SMS Delivery Route (e.g: 4)";
+$_ADDONLANG['flash'] = "Flash SMS (Yes=1/No=0)";
+$_ADDONLANG['unicode'] = "Unicode SMS (Yes=1/No=0)";
+$_ADDONLANG['ignoreNdnc'] = "Ignore NDNC Numbers? (Yes=1/No=0)";
